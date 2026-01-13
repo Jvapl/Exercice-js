c2d640a551676c8476f17110df0de8246acb1b89
joao aebi <joao.aebi@jobtrek.ch>
2026-01-13 11:03:19 +0100

    chore: computeAverage is validated

--- src/basics/maths.js
+++ src/basics/maths.js
@@ -7,6 +7,7 @@ import { ExpectTypeOf } from "vitest"
  * @param {number} diameter
  * @return {number} sphere volume
  */
+
 export function computeSphereVolume(diameter) {
   // Write your code here
   if (typeof diameter === "string" || diameter < 0 || Number.isNaN(diameter)){
@@ -16,33 +17,48 @@ export function computeSphereVolume(diameter) {
   const math = 4 / 3
   return math * Math.PI * radius**3 
 }
+
 /**
  * @param {number} n
  * @return {number} number rounded to one decimal
  */
+
 export function roundNumberToOneDecimals(n) {
   // Write your code here 
   if (typeof n !== "number" || Number.isNaN(n)){
   throw new Error ("Must be a valid number")
   }
   return Math.round(n * 10) / 10
-  
 }
 
 /**
  * @param {number[]} grades An array containing all grades
  * @return {number} average with full precision
  */
+
 export function computeAverage(grades) {
   // Write your code here
-
+  if (!Array.isArray(grades)){
+  throw new Error ("Must be a valid table")
+  }
+  for(const grade of grades){
+    if (typeof grade !== "number" || Number.isNaN(grade)){
+    throw new Error ("Must be a valid number")
+  }
+  }
+  let sum = 0
+  for (let i = 0 ; i < grades.length; i++) {
+    sum += grades[i]
+  }
+  
+  return sum/grades.length
 }
 
 /**
  * @param {number[]} grades An array containing all grades
  * @return {number} rounded average to 1 decimal
  */
+
 export function roundedAverage(grades) {
   // Write your code here
-
 }
