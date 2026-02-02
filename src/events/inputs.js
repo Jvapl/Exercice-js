65af6b4049a5bb0ff26d1f06fc4ad448ebc3c49d
joao aebi <joao.aebi@jobtrek.ch>
2026-02-02 11:45:40 +0100

    chore: addElementsInListOnEnterKey is validated

--- src/events/inputs.js
+++ src/events/inputs.js
@@ -5,26 +5,45 @@
  */
 export function displayInputContentInAlertOnEnterKey() {
   // Write your code here
-  const textImput = document.getElementById("write-some-text")
-  textImput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter" || textImput === "string"){
-      alert(`${textImput}`)
-    }if (event.key === "Enter" || textImput === " "){
-      alert(" ")
-    }else {
-      alert("Need to be a string")
+  const textInput = document.getElementById("write-some-text")
+
+  textInput.addEventListener("keydown", (event) => {
+    const content = textInput
+    if (event.key === "Enter" || textInput === "string" || textInput !== ""){
+      alert(textInput.value)
     }
   })
 }
-
 /**
  * On the page, you have an HTML input with the id "list-input".
  * The user can write text into it, and when he presses enter or blur the field,
  * the text should be added to a list of elements with id "list".
- */
+//  */  
 export function addElementsInListOnEnterKey() {
-  // Write your code here
+//   // Write your code here
+  const inputList = document.getElementById("list-input")
+  const nameList = document.getElementById("list")
+
+  const addElement = () => {
+    const text = inputList.value.trim();
+
+    if (text !== "") {
+      const newLi = document.createElement("li")
+      newLi.textContent = text;
+      nameList.appendChild(newLi)
+      inputList.vaule = ""
+    }
+  }
+  inputList.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      addElement()
+    }
+  });
+  inputList.addEventListener("blur", () => {
+    addElement();
+  })
 }
+
 
 /**
  * Add functionalities to the list. Now, when you click on one of the li, the element should be removed.
