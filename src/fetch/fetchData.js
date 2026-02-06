f995f89fcbedb2c7ed739efc5cc6ae2285ca9977
joao aebi <joao.aebi@jobtrek.ch>
2026-02-06 11:06:38 +0100

    chore: fetchDataOnClick is validated

--- src/fetch/fetchData.js
+++ src/fetch/fetchData.js
@@ -7,4 +7,15 @@
  */
 export function fetchDataOnClick() {
   // Write your code here
+  const buttonLink = document.getElementById("click-to-fetch")
+  const displayPre = document.getElementById("display-here")
+  buttonLink.addEventListener("click", () => {
+    fetch("https://api.github.com/octocat")
+    .then(reponse => { // 
+      return reponse.text()  // je transforme en texte
+    })
+    .then(information => {
+      displayPre.textContent = information // je mets mon texte dans le pre
+    })
+  })
 }
